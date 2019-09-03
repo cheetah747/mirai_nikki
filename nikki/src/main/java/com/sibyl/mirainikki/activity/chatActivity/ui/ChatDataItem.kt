9d9f3202af3453bec6d0ac08fb19ff76c4aaee2a
cheetah747 <cheetah747@qq.com>
2019-09-03 15:25:40 +0800

    chatItemの微調整完了

---
+++ nikki/src/main/java/com/sibyl/mirainikki/activity/chatActivity/ui/ChatDataItem.kt
@@ -0,0 +1,10 @@
+package com.sibyl.mirainikki.activity.chatActivity.ui
+
+/**
+ * @author Sasuke on 2019-9-3 0003.
+ */
+class ChatDataItem {
+    var msg: String = ""
+    var time: String = ""
+    var isMe = false
+}
